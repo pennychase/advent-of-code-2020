0ba5f4efae68902a3b84abe24f5012cc5d3305ea
Penny Chase <mpchase@mac.com>
2020-12-01 11:11:35 -0500

    Day 1 part 2 - generalized to combinations of any length

--- Day1.hs
+++ Day1.hs
@@ -5,23 +5,33 @@ import Data.List (subsequences, sort)
 readInt :: String -> Int
 readInt = read
 
-findSumEq :: Int -> [Int] -> Maybe (Int, Int)
-findSumEq x xs =
-    findSumEq' combinationsOfTwo
+combinationsOfLength :: Int -> [Int] -> [[Int]]
+combinationsOfLength n xs = filter (\y -> length y == n) $ subsequences (sort xs)
+
+-- find n numbers in xs (a list of numbers) whose sum is x
+-- Construct combinations of length n and then find the combination that sums to x
+findSumEq :: Int -> Int -> [Int] -> Maybe [Int]
+findSumEq n x xs =
+    findSumEq' $ combinationsOfLength n xs
     where
-        combinationsOfTwo = filter (\y -> length y == 2) $ subsequences (sort xs)
         findSumEq' [] = Nothing
-        findSumEq' ([n, m]:ns)
-            | n + m == x = Just (n, m)
-            | otherwise = findSumEq' ns
+        findSumEq' (m:ms)
+            | sum m == x = Just m
+            | otherwise = findSumEq' ms
 
 main :: IO ()
 main = do
     contents <- readFile "day-1-input.txt"
     let nums = map readInt $ lines contents
-    let result = findSumEq 2020 nums
+    -- Part 1
+    let result = findSumEq 2 2020 nums
     case result of
         Nothing     -> putStrLn "No pair sums to 2020"
-        Just (n,m)  -> putStrLn $ "The product of " ++ show (n,m) ++ " is " ++ show (n*m)
+        Just n  -> putStrLn $ "The product of " ++ show n ++ " is " ++ show (product n)
+    -- Part 2
+    let result' = findSumEq 3 2020 nums
+    case result' of
+        Nothing     -> putStrLn "No triple sums to 2020"
+        Just n  -> putStrLn $ "The product of " ++ show n ++ " is " ++ show (product n)
 
     
