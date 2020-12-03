93055148f8d3afd3600a40566a750afcecbb4f18
Penny Chase <mpchase@mac.com>
2020-12-03 15:27:51 -0500

    Day 3 - Added comments , renamed functions, and used foldr for part 2

--- src/Day3.hs
+++ src/Day3.hs
@@ -1,5 +1,7 @@
 module Day3 where
 
+-- Move through the grid using a slope given by Right and Down, counting the points
+-- that have '#'
 move :: Int -> Int -> Int -> Int -> [String] -> Int
 move row col right down grid 
     | newRow > length grid - 1 = 0
@@ -9,21 +11,18 @@ move row col right down grid
         newRow = row + down
         newCol = col + right
 
-part1 :: [String] -> Int
-part1 grid = move 0 0 3 1 grid
+-- Part 1: Moving through the grid with a single slope
+singleSlope :: (Int, Int) -> [String] -> Int
+singleSlope (r, d) = move 0 0 r d 
 
-part2 :: [String] -> Int
-part2 grid = 
-    move 0 0 1 1 grid *
-    move 0 0 3 1 grid *
-    move 0 0 5 1 grid *
-    move 0 0 7 1 grid *
-    move 0 0 1 2 grid 
-
+-- Part 2: Moving through the grid with a multiple slopes
+multipleSlopes :: [(Int, Int)] -> [String] -> Int
+multipleSlopes slopes grid = 
+    foldr (\s x -> singleSlope s grid * x) 1 slopes
 
 main :: IO ()
 main = do
     contents <- readFile "./test/data/day-3-input.txt"
     let grid = map cycle $ lines contents
-    print $ part1 grid
-    print $ part2 grid
+    print $ singleSlope (3, 1) grid
+    print $ multipleSlopes [(1,1), (3,1), (5,1), (7,1), (1, 2)] grid
