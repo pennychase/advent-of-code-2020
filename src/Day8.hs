db79f041b97db6f96de6730bf9864ee659f224ac
Penny Chase <mpchase@mac.com>
2020-12-09 10:07:46 -0500

    Day 8 part 2

--- src/Day8.hs
+++ src/Day8.hs
@@ -4,8 +4,8 @@ module Day8 where
 
 import qualified Data.Map as Map 
 import Data.Map ( Map )
-import Data.Void
-import Data.Text(Text)
+import Data.Void (Void)
+import Data.Text (Text)
 import qualified Data.Text as T
 import Text.Megaparsec ( Parsec, (<|>) )
 import qualified Text.Megaparsec as M
@@ -73,18 +73,35 @@ numberParser = read <$>
             M.char '+'
             integerParser
  
-mkComputer :: [Instruction] -> Computer
-mkComputer instrs =
+mkComputer :: Text -> Computer
+mkComputer input =
     Computer 0 0 (Map.fromList $ zip (cycle [0..]) instrs)
+    where
+        instrs = case M.runParser parseInstructions "" input of
+            Left s -> error (show s)
+            Right instrs -> instrs
 
-step :: Computer -> Computer
+
+changeInstr :: Computer -> Int -> Computer
+changeInstr comp index = 
+    case Map.lookup index (memory comp) of
+        Nothing -> error ("Illegal memory address: " ++ show index)
+        (Just instr) -> case opCode instr of
+            Jmp -> comp { memory = Map.insert index (instr { opCode = Nop }) (memory comp)  }
+            Nop -> comp { memory = Map.insert index (instr { opCode = Jmp}) (memory comp)  }
+            _   -> comp
+
+
+step :: Computer -> (String, Computer)
 step comp =
     case Map.lookup (pc comp) (memory comp) of
         Nothing -> 
             if pc comp == length (memory comp)
-                then comp
-                else error $ "Illegal memory address: " ++ show (pc comp)
-        (Just instr) -> if visited instr then comp else step (updateReg (updateMem comp instr) instr)
+                then ("terminates", comp)
+                else error ("Illegal memory address: " ++ show (pc comp))
+        (Just instr) -> if visited instr 
+                            then ("loops", comp)
+                            else step (updateReg (updateMem comp instr) instr)
     where
         updateReg c i = case opCode i of
             Nop -> c { pc = pc c + 1 }
@@ -93,15 +110,25 @@ step comp =
         updateMem c i  = c { memory = Map.insert (pc c) i { visited = True } (memory comp) }
             
 
-part1 :: Text -> Int
-part1 input = acc $ step comp
+part1 :: Text -> (String, Computer)
+part1 input = step (mkComputer input)
+
+part2 :: Text -> (String, Computer)
+part2 input =
+    go (Map.keys (memory comp))
     where
-        instrs = case M.runParser parseInstructions "" input of
-            Left s -> error (show s)
-            Right instrs -> instrs
-        comp = mkComputer instrs
+        comp = mkComputer input
+        go [] = ("doesn't terminate", comp)
+        go (i:is) =
+            case step (changeInstr comp i) of
+                ("loops", _) -> go is
+                ("terminates", c) -> ("terminates", c)
 
 main :: IO ()
 main = do
     content <- readFile "./test/data/Day-8-input.txt"
-    print $ part1 (T.pack content)
+    let (status, computer) = part1 (T.pack content)
+    print $ "Program " ++ status ++ " with acc = " ++ show (acc computer)
+    let (status, computer) = part2 (T.pack content)
+    print $ "Program " ++ status ++ " with acc = " ++ show (acc computer)
+
